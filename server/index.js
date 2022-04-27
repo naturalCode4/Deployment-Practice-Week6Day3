ba640d64a43226efc5447c304f35d9c0b52c0da9
Adin Kramer <fireowl@Adins-MacBook-Pro.local>
2022-04-27 17:06:13 -0600

    attempt to fix css

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
+app.use(express.static(path.join(__dirname, "../")));
+
 const port = process.env.PORT || 4005
 //does the alternative port (e.g. 4005) need to be anything in particular? 
 //is it set up so elsewhere?

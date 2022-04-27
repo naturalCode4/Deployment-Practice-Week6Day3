c29dfe32da60cb75dbc7f0afb5e4511f1b399926
Adin Kramer <fireowl@Adins-MacBook-Pro.local>
2022-04-27 14:39:24 -0600

    second commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+const port = process.env.PORT || 4005
+//does the alternative port (e.g. 4005) need to be anything in particular? 
+//is it set up so elsewhere?
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
+
+console.log(path)
